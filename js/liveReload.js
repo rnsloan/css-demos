e6bef6e0f99dffb6863e08d9130fac9fe47b6bf9
rnsloan <rnsloan1@gmail.com>
2013-11-20 15:26:09 +0000

    Add LiveReload

---
+++ js/liveReload.js
@@ -0,0 +1,9 @@
+var active = true;
+
+if (active) {
+    runLiveReload();
+}
+
+function runLiveReload() {
+    document.write('<script src="http://' + (location.host || 'localhost').split(':')[0] + ':35729/livereload.js?snipver=1"></' + 'script>');
+}
